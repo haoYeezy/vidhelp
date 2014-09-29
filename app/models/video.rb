9c366ab4fb80775b9175fea61e62e0f2a863c38d
Hao Yi <hao.yi@live.ca>
2014-09-28 21:38:32 -0400

    controller update

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,11 @@
 class Video < ActiveRecord::Base
-	belongs_to :user
-	scope :recentvideos, order('created_at desc')
-	scope :unseen, where(seen_at: nil) 
+belongs_to :user
+before_save :check_sender
+scope :recentvideos, order('created_at desc')
+scope :unseen, where(seen_at: nil) 
+
+	def check_sender
+		if self.sender?
+		self.sender = userid	
+	end	
 end
