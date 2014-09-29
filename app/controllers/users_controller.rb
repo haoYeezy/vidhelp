7cf0706cd30de0a0e4fd57d9ce7acd5a4bafb290
Hao Yi <hao.yi@live.ca>
2014-09-28 22:59:52 -0400

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
+  
+  def index
+  	if params[:status] == "activated"
+  		@users = User.activated
+  	else
+  		@users = User.inactivated
+  	end
+  end	
+
   def new
+  end
+  	
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		redirect_to @user
+  	else
+  		render "new"
+  	end
   end
 
   def edit
+  end
+
+  def save
+  	@user = User.save
   end
 
   def show
